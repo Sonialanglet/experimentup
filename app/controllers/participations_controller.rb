f87206b86a619657cf405efa1492b1f482bb6a6f
Sonialanglet <slanglet@loeildumarketing.com>
2019-03-12 09:39:59 +0100

    modif des places disponibles et du controller participation

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -11,9 +11,9 @@ class ParticipationsController < ApplicationController
 
     @participation = Participation.find(params[:id])
     authorize @participation
-
-
-
+    @event_id = @participation.event_id
+    @event=Event.where(id:@event_id).first
+    @places_dispo = @event.number_of_participants - @event.participations.where(status:'payé').sum(:quantity)
   end
 
 
@@ -23,15 +23,20 @@ class ParticipationsController < ApplicationController
     @event = Event.find(params[:event_id])
   end
 
+
   def create
     @event = Event.find(params[:event_id])
     @participation = Participation.new(participation_params)
 
     @participation.user = current_user
+    @participation.first_name = current_user.first_name
+    @participation.last_name = current_user.first_name
+    @participation.email = current_user.email
     @participation.event_ref = @event.name
+    @participation.event_id = @event.id
     @participation.amount_cents = @event.price_cents * @participation.quantity
     authorize @participation
-      @places_dispo = @event.number_of_participants - Participation.where(status:'payé').sum(:quantity) - @participation.quantity
+      @places_dispo = @event.number_of_participants - @event.participations.where(status:'payé').sum(:quantity) - @participation.quantity
        if @places_dispo > 0
          @participation.save
           redirect_to new_participation_charge_path(@participation)
@@ -49,7 +54,7 @@ class ParticipationsController < ApplicationController
   private
 
     def participation_params
-      params.require(:participation).permit(:first_name, :last_name, :email, :phone, :status, :quantity, :event_ref, :amount_cents, :user, :available_places)
+      params.require(:participation).permit(:first_name, :last_name, :email, :phone, :status, :quantity, :event_ref, :amount_cents, :user, :available_places, :event_id)
     end
 
 end
